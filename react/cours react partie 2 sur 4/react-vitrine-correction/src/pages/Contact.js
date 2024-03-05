628bcc46ad3f570e069f1b8be474ff67c0b3373f
ljere76 <ljeremie@legtux.org>
2024-03-05 20:09:29 +0100

    ""

--- react/cours react partie 2 sur 4/react-vitrine-correction/src/pages/Contact.js
+++ react/cours react partie 2 sur 4/react-vitrine-correction/src/pages/Contact.js
@@ -1,50 +1,69 @@
-import Navigation from "../components/Navigation";
-import Mouse from "../components/Mouse";
-import Logo from "../components/Logo";
-import Buttons from "../components/Buttons";
+import React from "react";
 import ContactForm from "../components/ContactForm";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import SocialNetwork from "../components/SocialNetwork";
+import Buttons from "../components/Buttons";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 200,
+    },
+  };
+
   return (
     <main>
       <Mouse />
-      <div className="contact">
+      <motion.div
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransition}
+        transition={{ duration: 0.4 }}
+        className="contact"
+      >
         <Navigation />
         <Logo />
         <ContactForm />
         <div className="contact-infos">
           <div className="address">
             <div className="content">
-              <h4>Adresse</h4>
-              <p>1 Avenue du Général de Gaulle</p>
-              <p>75015 Paris</p>
+              <h4>adresse</h4>
+              <p>12 rue Laplace</p>
+              <p>64200 Biarritz</p>
             </div>
           </div>
-
           <div className="phone">
             <div className="content">
-              <h4>Téléphone</h4>
-              <CopyToClipboard text="01 23 45 67 89" className="hover">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0642844284" className="hover">
                 <p
                   style={{ cursor: "pointer" }}
                   className="clipboard"
-                  onClick={() => alert("Téléphone copie !")}
+                  onClick={() => alert("Téléphone copié !")}
                 >
-                  01 23 45 67 89
+                  06 42 84 42 84
                 </p>
               </CopyToClipboard>
             </div>
           </div>
           <div className="email">
             <div className="content">
-              <h4>Email</h4>
+              <h4>email</h4>
               <CopyToClipboard text="fsagency@gmail.com" className="hover">
                 <p
                   style={{ cursor: "pointer" }}
                   className="clipboard"
-                  onClick={() => alert("Email copie !")}
+                  onClick={() => alert("Email copié !")}
                 >
                   fsagency@gmail.com
                 </p>
@@ -57,7 +76,7 @@ const Contact = () => {
           </div>
         </div>
         <Buttons left={"/projet-4"} />
-      </div>
+      </motion.div>
     </main>
   );
 };
